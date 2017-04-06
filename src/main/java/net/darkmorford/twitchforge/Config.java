f9945ac0aa55499512c29db7839f31868d95ccf7
DarkMorford <DarkMorford@users.noreply.github.com>
2017-04-06 11:46:26 -0700

    Add a basic configuration file.

---
+++ src/main/java/net/darkmorford/twitchforge/Config.java
@@ -0,0 +1,41 @@
+package net.darkmorford.twitchforge;
+
+import net.darkmorford.twitchforge.proxy.CommonProxy;
+import net.minecraftforge.common.config.Configuration;
+import org.apache.logging.log4j.Level;
+
+public class Config
+{
+    // Configuration categories
+    private static final String CATEGORY_GENERAL = "general";
+
+    // General configuration values
+    public static String twitchChannel = "minecraft";
+    public static int updateInterval = 5;
+
+    public static void readConfig()
+    {
+        Configuration cfg = CommonProxy.config;
+        try
+        {
+            cfg.load();
+            initGeneralConfig(cfg);
+        }
+        catch (Exception e)
+        {
+            TwitchForge.logger.log(Level.ERROR, "Problem loading config file!", e);
+        }
+        finally
+        {
+            if (cfg.hasChanged())
+                cfg.save();
+        }
+    }
+
+    private static void initGeneralConfig(Configuration cfg)
+    {
+        cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
+        twitchChannel = cfg.getString("twitchChannel", CATEGORY_GENERAL, twitchChannel, "Set the twitch channel to watch");
+        updateInterval = cfg.getInt("updateInterval", CATEGORY_GENERAL, updateInterval, 1, 60, "Set the update interval");
+    }
+}
--- src/main/java/net/darkmorford/twitchforge/proxy/CommonProxy.java
+++ src/main/java/net/darkmorford/twitchforge/proxy/CommonProxy.java
@@ -1,13 +1,22 @@
 package net.darkmorford.twitchforge.proxy;
 
+import net.darkmorford.twitchforge.Config;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import java.io.File;
+
 public class CommonProxy
 {
+    public static Configuration config;
+
     public void preInit(FMLPreInitializationEvent event)
     {
+        File configDir = event.getModConfigurationDirectory();
+        config = new Configuration(new File(configDir.getPath(), "twitchforge.cfg"));
+        Config.readConfig();
     }
 
     public void init(FMLInitializationEvent event)
@@ -16,5 +25,7 @@ public class CommonProxy
 
     public void postInit(FMLPostInitializationEvent event)
     {
+        if (config.hasChanged())
+            config.save();
     }
 }
