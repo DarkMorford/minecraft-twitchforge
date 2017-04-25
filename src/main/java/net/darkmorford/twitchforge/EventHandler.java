f656525a2c2d754ee94986e4d8610b66656d39a3
DarkMorford <DarkMorford@users.noreply.github.com>
2017-04-25 09:26:01 -0700

    Properly subscribe to the PlayerLoggedIn event.

--- src/main/java/net/darkmorford/twitchforge/EventHandler.java
+++ src/main/java/net/darkmorford/twitchforge/EventHandler.java
@@ -3,17 +3,20 @@ package net.darkmorford.twitchforge;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 @Mod.EventBusSubscriber
 public class EventHandler
 {
+    @SubscribeEvent
     public static void playerLoggedIn(PlayerEvent.PlayerLoggedInEvent event)
     {
         // Find out who just logged in
         EntityPlayer player = event.player;
+        String playerName = player.getDisplayNameString();
 
         // Send them a welcome message
-        player.addChatComponentMessage(new TextComponentTranslation("misc.motd", player.getDisplayNameString()));
+        player.addChatComponentMessage(new TextComponentTranslation("misc.motd", playerName));
     }
 }
