86d1435468646cc1f819b8d6789b58c430f06afb
DarkMorford <DarkMorford@users.noreply.github.com>
2017-05-12 09:04:57 -0700

    Change block state when stream goes online/offline.

--- src/main/java/net/darkmorford/pleasewait/tileentity/TileStreamDetector.java
+++ src/main/java/net/darkmorford/pleasewait/tileentity/TileStreamDetector.java
@@ -1,11 +1,17 @@
 package net.darkmorford.pleasewait.tileentity;
 
+import net.darkmorford.pleasewait.twitch.TwitchState;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import net.minecraft.world.World;
 
+import static net.darkmorford.pleasewait.block.BlockStreamDetector.PROPERTYACTIVE;
+
 public class TileStreamDetector extends TileEntity implements ITickable
 {
+    private boolean activeLastTick = false;
+
     @Override
     public void update()
     {
@@ -17,5 +23,22 @@ public class TileStreamDetector extends TileEntity implements ITickable
         World world = getWorld();
         if (world.isRemote)
             return;
+
+        boolean currentlyActive = TwitchState.isStreamOnline;
+        IBlockState currentState = world.getBlockState(getPos());
+
+        if (!activeLastTick && currentlyActive)
+        {
+            // Stream just came online, turn on redstone signal
+            world.setBlockState(getPos(), currentState.withProperty(PROPERTYACTIVE, true));
+        }
+
+        if (activeLastTick && !currentlyActive)
+        {
+            // Stream just went down, turn off signal
+            world.setBlockState(getPos(), currentState.withProperty(PROPERTYACTIVE, false));
+        }
+
+        activeLastTick = currentlyActive;
     }
 }
